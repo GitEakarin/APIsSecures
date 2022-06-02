947ac3f78c8683db4fb76f438f72d8f63e0fd4c9
GitEakarin <88380841+GitEakarin@users.noreply.github.com>
2022-06-02 16:04:24 +0700

    Add files via upload

---
+++ APIsSecures/Models/LoginModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIsSecures.Models
+{
+    public class LoginModel
+    {
+        [Required]
+        public string User { get; set; }
+        [Required]
+        public string Pass { get; set; }
+        //public string Token { get; set; }
+    }
+}
