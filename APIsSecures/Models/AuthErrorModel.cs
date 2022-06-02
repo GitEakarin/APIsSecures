2bfbfb16dafcd764d8e195204be232d879e91d5a
GitEakarin <88380841+GitEakarin@users.noreply.github.com>
2022-06-02 16:04:08 +0700

    Create AuthErrorModel.cs

---
+++ APIsSecures/Models/AuthErrorModel.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIsSecures.Models
+{
+    public class AuthErrorModel
+    {
+        public string ErrMsg { get; set; }
+    }
+}
